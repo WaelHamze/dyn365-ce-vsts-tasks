d1fbfa95c28654b5e2710d2407a97ec70f98009b
Wael Hamze <contact@waelhamze.com>
2018-09-25 18:25:41 +0100

    Added Support for Async Stage for Upgrade Solution Import

--- BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMApplySolution/MSCRMApplySolution.ps1
+++ BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMApplySolution/MSCRMApplySolution.ps1
@@ -9,11 +9,15 @@ Write-Verbose 'Entering MSCRMApplySolution.ps1'
 #Get Parameters
 $crmConnectionString = Get-VstsInput -Name crmConnectionString -Require
 $solutionName = Get-VstsInput -Name solutionName -Require
+$useAsyncMode = Get-VstsInput -Name useAsyncMode -Require -AsBool
+$asyncWaitTimeout = Get-VstsInput -Name asyncWaitTimeout -Require -AsInt
 $crmConnectionTimeout = Get-VstsInput -Name crmConnectionTimeout -Require -AsInt
 
 #Print Verbose
 Write-Verbose "crmConnectionString = $crmConnectionString"
 Write-Verbose "solutionName = $solutionName"
+Write-Verbose "useAsyncMode = $useAsyncMode"
+Write-Verbose "asyncWaitTimeout = $asyncWaitTimeout"
 Write-Verbose "crmConnectionTimeout = $crmConnectionTimeout"
 
 #MSCRM Tools
@@ -25,6 +29,6 @@ if (-not $mscrmToolsPath)
 	Write-Error "MSCRM_Tools_Path not found. Add 'MSCRM Tool Installer' before this task."
 }
 
-& "$mscrmToolsPath\xRMCIFramework\9.0.0\ApplySolution.ps1" -solutionName "$solutionName" -crmConnectionString "$CrmConnectionString" -Timeout $crmConnectionTimeout
+& "$mscrmToolsPath\xRMCIFramework\9.0.0\ApplySolution.ps1" -solutionName "$solutionName" -crmConnectionString "$CrmConnectionString" -ImportAsync $useAsyncMode -AsyncWaitTimeout $asyncWaitTimeout -Timeout $crmConnectionTimeout
 
 Write-Verbose 'Leaving MSCRMApplySolution.ps1'
