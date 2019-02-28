3e977b53873a4120dd94ec37fc43d0640e0fc152
Wael Hamze <contact@waelhamze.com>
2019-02-28 07:34:36 +0000

    Added links to example config files

--- BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMToolInstaller/MSCRMToolInstaller.ps1
+++ BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMToolInstaller/MSCRMToolInstaller.ps1
@@ -25,7 +25,7 @@ Write-Host "Using Tools Path: $toolPath"
 $frameworkCache = $toolPath + "\MSCRMBuildTools"
 Write-Verbose "Framework Cache: $frameworkCache"
 
-$currentVersion = '9.0.7'
+$currentVersion = '9.0.8'
 $currentVersionPath = "$frameworkCache\$currentVersion"
 
 Write-Host "Tools Version: $currentVersion"
