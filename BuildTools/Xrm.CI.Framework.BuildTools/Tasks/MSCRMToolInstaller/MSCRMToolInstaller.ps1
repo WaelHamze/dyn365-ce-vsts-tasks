90eca4fd9ae80ec96c564ae4f68449da24ba57da
Wael Hamze <contact@waelhamze.com>
2019-04-17 15:28:26 +0100

    Improved Logging

--- BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMToolInstaller/MSCRMToolInstaller.ps1
+++ BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMToolInstaller/MSCRMToolInstaller.ps1
@@ -25,7 +25,7 @@ Write-Host "Using Tools Path: $toolPath"
 $frameworkCache = $toolPath + "\MSCRMBuildTools"
 Write-Verbose "Framework Cache: $frameworkCache"
 
-$currentVersion = '9.0.12'
+$currentVersion = '9.0.14'
 $currentVersionPath = "$frameworkCache\$currentVersion"
 
 Write-Host "Tools Version: $currentVersion"
