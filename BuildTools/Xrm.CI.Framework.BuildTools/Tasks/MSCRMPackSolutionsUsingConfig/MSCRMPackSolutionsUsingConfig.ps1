69c5f12c25e6cab2e3a77a07013ac52ccb1285f6
Wael Hamze <contact@waelhamze.com>
2020-03-06 08:56:50 +0000

    Added capability to select specific version of the dependency modules

--- BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMPackSolutionsUsingConfig/MSCRMPackSolutionsUsingConfig.ps1
+++ BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMPackSolutionsUsingConfig/MSCRMPackSolutionsUsingConfig.ps1
@@ -10,7 +10,6 @@ Write-Verbose 'Entering MSCRMPackSolutionsUsingConfig.ps1'
 $configFilePath = Get-VstsInput -Name configFilePath -Require
 $outputFolder = Get-VstsInput -Name outputFolder -Require
 $logsDirectory = Get-VstsInput -Name logsDirectory
-$solutionpackagerpath = Get-VstsInput -Name solutionpackagerpath
 
 #MSCRM Tools
 $mscrmToolsPath = $env:MSCRM_Tools_Path
@@ -31,19 +30,19 @@ if (-not $logsDirectory)
 	Write-Verbose "logsDirectory set to $logsDirectory"
 }
 
-if ((-not $solutionpackagerpath) -or ($solutionpackagerpath -eq $env:System_DefaultWorkingDirectory))
-{
-	Write-Verbose "solutionpackagerpath not supplied"
-	
-	$solutionpackagerpath = "$mscrmToolsPath\CoreTools\9.0.0\SolutionPackager.exe"
+."$mscrmToolsPath\MSCRMToolsFunctions.ps1"
 
-	Write-Verbose "logFileName set to $solutionpackagerpath"
-}
+Require-ToolsTaskVersion -version 10
+
+$coreTools = 'Microsoft.CrmSdk.CoreTools'
+$coreToolsInfo = Get-MSCRMToolInfo -toolName $coreTools
+$coreToolsPath = "$($coreToolsInfo.Path)\content\bin\coretools"
+Use-MSCRMTool -toolName $coreTools -version $coreToolsInfo.Version
 
 #Import
 try
 {
-	& "$mscrmToolsPath\xRMCIFramework\9.0.0\PackSolutionsUsingConfig.ps1" -configFilePath "$configFilePath" -solutionPackagerPath "$solutionPackagerPath" -OutputFolder "$outputFolder" -logsDirectory "$logsDirectory"
+	& "$mscrmToolsPath\xRMCIFramework\9.0.0\PackSolutionsUsingConfig.ps1" -configFilePath "$configFilePath" -CoreToolsPath "$coreToolsPath" -OutputFolder "$outputFolder" -logsDirectory "$logsDirectory"
 }
 catch
 {
