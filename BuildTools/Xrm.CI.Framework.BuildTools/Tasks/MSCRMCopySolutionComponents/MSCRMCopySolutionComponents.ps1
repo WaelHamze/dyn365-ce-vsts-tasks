f08c2bbcf70b1e0f3ea32e06433751206383de58
mtulodzi <michal@tulodziecki.pl>
2022-03-18 15:09:58 +0100

    Adding an optional parameter to include components from source solution patches

--- BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMCopySolutionComponents/MSCRMCopySolutionComponents.ps1
+++ BuildTools/Xrm.CI.Framework.BuildTools/Tasks/MSCRMCopySolutionComponents/MSCRMCopySolutionComponents.ps1
@@ -10,12 +10,14 @@ Write-Verbose 'Entering MSCRMCopySolutionComponents.ps1'
 $crmConnectionString = Get-VstsInput -Name crmConnectionString -Require
 $fromSolutionName = Get-VstsInput -Name fromSolutionName -Require
 $toSolutionName = Get-VstsInput -Name toSolutionName -Require
+$includePatches = Get-VstsInput -Name includePatches -AsBool
 $crmConnectionTimeout = Get-VstsInput -Name crmConnectionTimeout -Require -AsInt
 
 #Print Verbose
 Write-Verbose "crmConnectionString = $crmConnectionString"
 Write-Verbose "fromSolutionName = $fromSolutionName"
 Write-Verbose "toSolutionName = $toSolutionName"
+Write-Verbose "includePatches = $includePatches"
 Write-Verbose "crmConnectionTimeout = $crmConnectionTimeout"
 
 #MSCRM Tools
@@ -27,6 +29,6 @@ if (-not $mscrmToolsPath)
 	Write-Error "MSCRM_Tools_Path not found. Add 'Power DevOps Tool Installer' before this task."
 }
 
-& "$mscrmToolsPath\xRMCIFramework\9.0.0\CopySolutionComponents.ps1" -fromSolutionName "$fromSolutionName" -toSolutionName "$toSolutionName" -crmConnectionString "$CrmConnectionString" -Timeout $crmConnectionTimeout
+& "$mscrmToolsPath\xRMCIFramework\9.0.0\CopySolutionComponents.ps1" -fromSolutionName "$fromSolutionName" -toSolutionName "$toSolutionName" -includePatches $includePatches -crmConnectionString "$CrmConnectionString" -Timeout $crmConnectionTimeout
 
 Write-Verbose 'Leaving MSCRMCopySolutionComponents.ps1'
